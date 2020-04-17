4b6876fee1bde189ec59df0a74bc5c3cedf2a2f3
Ra <rasinmik@hotmail.com>
2020-04-17 15:46:24 +0300

    changed my name to window title

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) {
-        stage.setTitle("JavaFX HelloWorld!");
+        stage.setTitle("JavaFX Mika!");
         stage.show();
     }
     public static void main(String args[]) {
