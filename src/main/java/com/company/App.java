730a0daf9992bbedd43c4f4beb65d2c9075c7885
Ra <rasinmik@hotmail.com>
2020-04-17 16:07:46 +0300

    change title

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) {
-        stage.setTitle("JavaFX Mika!");
+        stage.setTitle("JavaFX FX!");
         stage.show();
     }
     public static void main(String args[]) {
