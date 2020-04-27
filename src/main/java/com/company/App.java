6c89981c110689af147e67b1dfb1a08b11144dce
Ra <rasinmik@hotmail.com>
2020-04-27 18:53:30 +0300

    update

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,15 +1,338 @@
 
 package com.company;
 
+import com.company.util.FileHandler;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.control.*;
+//import javafx.scene.control.MenuItemBuilder;
+
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.layout.BorderPane;
+
+import javax.swing.*;
+import java.io.File;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class App extends Application {
+
+    private TextArea textArea;
+    private FileHandler fileHandler;
+    private MenuItem fileOpen;
+    private MenuItem fileSave;
+    private String path;
+
+    public App(){
+        System.out.println("Constructor");
+    }
+
+    public void saveAs(ActionEvent e){
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save File");
+        File f = fileChooser.showSaveDialog(new Stage());
+
+        try {
+            if (f != null) {
+                String path = f.getAbsolutePath();
+                fileHandler.setFilePath(path);
+                fileHandler.saveFile(textArea.getText());
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            Alert alertSave = new Alert(Alert.AlertType.ERROR);
+            alertSave.setTitle("Error Dialog");
+            alertSave.setHeaderText("Something go wrong this time.");
+            alertSave.setContentText(ex.getMessage());
+            alertSave.showAndWait();
+        }
+            System.out.println("Save");
+            //fileHandler.setFilePath("/home/ra/Test/Test.java");
+            //fileHandler.saveFile(textArea.getText());
+
+    }
+
+    public void save(ActionEvent e){
+
+        //FileChooser fileChooser = new FileChooser();
+        //fileChooser.setTitle("Save File");
+        //File f = fileChooser.showSaveDialog(new Stage());
+        //if (f != null) {
+        //    String path = f.getAbsolutePath();
+            fileHandler.setFilePath(path);
+            //fileHandler.saveFile(textArea.getText());
+        //}
+        System.out.println("Save");
+        //fileHandler.setFilePath("/home/ra/Test/Test.java");
+        //fileHandler.saveFile(textArea.getText());
+
+    }
+
+    public void open(ActionEvent e){
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open File");
+
+        //File f = fileChooser.showOpenDialog(fileOpen.getParentPopup().getScene().getWindow());
+        File f = fileChooser.showOpenDialog(null);
+
+        try {
+            if (f == null) {
+                System.out.println("Cancel");
+                return;
+            } else {
+                path = f.getAbsolutePath();
+                fileHandler.setFilePath(path);
+                String content = fileHandler.openFile();
+                textArea.setText(content);
+                fileSave.setDisable(false);
+            }
+        } catch (Exception except) {
+            System.out.println(except.getMessage());
+            Alert open = new Alert(Alert.AlertType.ERROR);
+            open.setTitle("Error Dialog");
+            open.setHeaderText("Something go wrong this time.");
+            open.setContentText(except.getMessage());
+            open.showAndWait();
+        }
+        System.out.println("Open");
+        //fileHandler.setFilePath("/home/ra/Test/Test.java");
+        //String content = fileHandler.openFile();
+        //textArea.setText(content);
+    }
+
     @Override
     public void start(Stage stage) {
-        stage.setTitle("JavaFX FX!");
+
+        fileHandler = new FileHandler();
+        SplitPane splitpane = new SplitPane();
+        splitpane.setOrientation(Orientation.VERTICAL);
+
+        Locale locale = Locale.getDefault();
+        //Locale locale = new Locale("fi", "FI");
+        ResourceBundle labels = ResourceBundle.getBundle("ui", locale);
+        String title = labels.getString("title");
+
+        BorderPane root = new BorderPane();
+
+
+        FileChooser fileChooser = new FileChooser();
+
+        Menu mFile = new Menu(labels.getString("file"));
+
+        MenuItem fileNew = new MenuItem(labels.getString("new"));
+        fileNew.setAccelerator(new KeyCodeCombination(KeyCode.N, KeyCombination.CONTROL_DOWN));
+        mFile.getItems().add(fileNew);
+
+        fileOpen = new MenuItem(labels.getString("open"));
+        fileOpen.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN));
+        fileOpen.setOnAction(this::open);
+        mFile.getItems().add(fileOpen);
+
+        MenuItem fileSaveAs = new MenuItem(labels.getString("save_as"));
+        fileSaveAs.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.SHIFT_DOWN, KeyCombination.CONTROL_DOWN));
+        mFile.getItems().add(fileSaveAs);
+        fileSaveAs.setOnAction(this::saveAs);
+
+        fileSave = new MenuItem(labels.getString("save"));
+        fileSave.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN));
+        mFile.getItems().add(fileSave);
+        fileSave.setDisable(true);
+        fileSave.setOnAction(this::save);
+
+        SeparatorMenuItem separator = new SeparatorMenuItem();
+        mFile.getItems().add(separator);
+
+        MenuItem fileExit = new MenuItem(labels.getString("exit"));
+        mFile.getItems().add(fileExit);
+
+        fileExit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Exit");
+                System.exit(0);
+            }
+        });
+
+        textArea = new TextArea(("Center"));
+        textArea.setStyle("-fx-font-size: 12");
+
+        // EDIT MENU
+        Menu mEdit = new Menu(labels.getString("edit"));
+        MenuItem editCut = new MenuItem(labels.getString("cut"));
+        editCut.setAccelerator(new KeyCodeCombination(KeyCode.X, KeyCombination.CONTROL_DOWN));
+        mEdit.getItems().add(editCut);
+
+        MenuItem editCopy = new MenuItem(labels.getString("copy"));
+        editCopy.setAccelerator(new KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_DOWN));
+        mEdit.getItems().add(editCopy);
+
+        MenuItem editPaste = new MenuItem(labels.getString("paste"));
+        editPaste.setAccelerator(new KeyCodeCombination(KeyCode.V, KeyCombination.CONTROL_DOWN));
+        mEdit.getItems().add(editPaste);
+
+        editCut.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                textArea.cut();
+            }
+        });
+
+        editCopy.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                textArea.copy();
+            }
+        });
+
+        editPaste.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                textArea.paste();
+            }
+        });
+
+        // RUN MENU
+        Menu mRun = new Menu(labels.getString("run"));
+        MenuItem runItem1 = new MenuItem(labels.getString("compile_run"));
+        mRun.getItems().add(runItem1);
+
+        // ABOUT MENU
+        Menu mAbout = new Menu(labels.getString("about"));
+        MenuItem aboutItem1 = new MenuItem(labels.getString("about_app"));
+        mAbout.getItems().add(aboutItem1);
+
+        aboutItem1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Alert info = new Alert(Alert.AlertType.INFORMATION);
+                info.setTitle("Information Dialog");
+                info.setHeaderText("About");
+                info.setContentText("Mika");
+
+                info.showAndWait();
+            }
+        });
+
+        // Add components to menubar
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(mFile, mEdit, mRun, mAbout);
+
+        //VBox vBox = new VBox(menuBar);
+
+        // Toolbar
+        ToolBar toolBar = new ToolBar();
+        Button button = new Button("Clear");
+        toolBar.getItems().add(button);
+
+        ComboBox comboFont = new ComboBox(FXCollections.observableList(Font.getFamilies()));
+        //comboFont.setPrefWidth(150);
+        comboFont.getSelectionModel().select(1);
+        comboFont.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                textArea.setStyle("-fx-font-family: " + comboFont.getValue() + ";");
+            }
+        });
+        toolBar.getItems().add(comboFont);
+
+        TextField fontSize = new TextField("12");
+
+        fontSize.setPrefColumnCount(2);
+
+        //ComboBox fontSize = new ComboBox(FXCollections.observableArrayList(koko));
+        fontSize.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                textArea.setStyle("-fx-font-size: " + fontSize.getText() + ";");
+            }
+        });
+        toolBar.getItems().add(fontSize);
+
+
+        // Colorpicker
+        ColorPicker colorPicker = new ColorPicker();
+        //Color value = colorPicker.getValue();
+
+
+        colorPicker.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                textArea.setStyle("-fx-text-fill: " + toRgbString(colorPicker.getValue()) + ";");
+            }
+        });
+
+        toolBar.getItems().add(colorPicker);
+
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Clear");
+                textArea.clear();
+            }
+        });
+
+        splitpane.getItems().addAll(textArea, new TextArea("down"));
+        //bPane.setCenter(text);
+
+        textArea.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.TAB){
+                    System.out.println("....");
+                    int index = textArea.getCaretPosition();
+                    textArea.replaceText(index-1, index,"    ");
+                }
+            }
+        });
+
+        VBox vBox = new VBox(menuBar, toolBar);
+        root.setTop(vBox);
+        root.setCenter(splitpane);
+
+        Scene scene = new Scene(root, 640, 480);
+
+        stage.setTitle(title);
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setScene(scene);
         stage.show();
     }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        System.out.println("Stop");
+    }
+
+    public String toRgbString(Color c){
+        return "rgb("
+                + to255Int(c.getRed())
+                + "," + to255Int(c.getGreen())
+                + "," + to255Int(c.getBlue())
+                + ")";
+    }
+
+    public int to255Int(double d){
+        return (int) (d * 255);
+    }
+
+
+
     public static void main(String args[]) {
         launch(args);
     }
---
+++ src/main/java/com/company/util/FileHandler.java
@@ -0,0 +1,55 @@
+
+// Create new package for this
+package com.company.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileHandler {
+
+    private String filePath;
+
+    public FileHandler(String filePath) {
+        setFilePath(filePath);
+    }
+
+    public FileHandler() {}
+
+    // create getter and setter for filePath!
+    public void setFilePath(String filePath){
+        this.filePath = filePath;
+    }
+
+    public String getFilePath(){
+        return filePath;
+    }
+
+
+    // Opens the given file and returns the content, uses filePath
+    public String openFile() throws Exception{
+
+        String content = "";
+
+        try {
+            content = Files.readString(Paths.get(filePath));
+        } catch (IOException e) {
+            throw new IOException("Can't save a file!");
+            //System.out.println("problem with IO");
+            //e.printStackTrace();
+        }
+        return content;
+    }
+
+    // Saves the content to given file path
+    public void saveFile(String content) throws Exception{
+
+        try {
+            Files.writeString(Paths.get(filePath), content);
+        } catch(IOException e) {
+            throw new IOException("Can't save a file!");
+            //System.out.println("problem with IO");
+            //e.printStackTrace();
+        }
+    }
+}
